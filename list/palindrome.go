df5ff4d3664dedf224122ff38ce6d9941bf8ddd9
dlstonedl <ldeng@thoughtworks.com>
2019-04-28 21:55:00 +0800

    palindrome

---
+++ list/palindrome.go
@@ -0,0 +1,48 @@
+package list
+
+func isPalindrome(head *ListNode) bool {
+	if head == nil {
+		return true
+	}
+
+	if head.Next == nil {
+		return true
+	}
+
+	if head.Next.Next == nil {
+		if head.Val == head.Next.Val {
+			return true
+		}
+
+		return false
+	}
+
+	fast, low := head, head
+	var values []int
+	values = append(values, low.Val)
+
+	for fast.Next != nil && fast.Next.Next != nil {
+		low = low.Next
+		values = append(values, low.Val)
+		fast = fast.Next.Next
+	}
+
+	if fast.Next != nil {
+		low = low.Next
+	}
+
+	for low != nil {
+		if low.Val != values[len(values)-1] {
+			return false
+		} else {
+			values = values[0 : len(values)-1]
+		}
+		low = low.Next
+	}
+
+	if len(values) != 0 {
+		return false
+	}
+
+	return true
+}
